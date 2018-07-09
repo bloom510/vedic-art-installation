3120398dc6d248ede1ac829cc50d26fcbb052dbb
bloom510 <bloomjazzer@gmail.com>
2018-07-08 23:59:22 -0700

    initial commit

--- vedic.js
+++ vedic.js
@@ -7,34 +7,26 @@ class VedicSquare {
     this.scale = Dictionary
     .scale(scale)
     .map(Transpose.transpose(`${key}2`));
-
     this.table = [];
     this.populate(modulus)  
-    console.log(this.table)
-  }
-  populate(modulus){
-    this.populate(modulus);
   }
   populate(modulus){
     let cell;
     for(let i = 1; i <= modulus; i++){
       this.table[i - 1] = [];
       for(let j = 1; j <= modulus; j++){
-          if ((1 + (i*j) - 1) % modulus === 0) {
+        if((1 + (i*j) - 1) % modulus === 0) {
             cell = this.scale[(modulus - 1) % this.scale.length];
             this.table[i - 1].push(cell);
-            } else {
-            cell = ((1 + (i*j) - 1) % modulus)
-            this.table[i - 1].push(
-              this.scale[
-                (cell - 1) % this.scale.length
-              ]
-            );
-          }
+        } else {
+          cell = ((1 + (i*j) - 1) % modulus)
+          this.table[i - 1].push(
+            this.scale[(cell - 1) % this.scale.length]
+          );
+        }
       }
     }
   }
-
 }
 
 new VedicSquare(9, 'major', 'C')
