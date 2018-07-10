fbde44e9c6ceeab450fdfc3bf5df4d22e8004b28
bloom510 <bloomjazzer@gmail.com>
2018-07-09 21:26:42 -0700

    Programming playback

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,18 @@ router.get('/', (req, res, next) => {
   res.render('index', { 
     title: 'Express', scripts: 
       [
+        {script: '../javascripts/playback.js'},
+         //Business logic
+         {script: '../javascripts/canvas.js'},
         //Socket.io 
         {script: '/socket.io/socket.io.js'},
         {script: '../javascripts/client.js'},
-        //jQuery
-        {script: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'},
         //Tone.js
-        {script: 'https://cdnjs.cloudflare.com/ajax/libs/tone/13.2.0/Tone.js'},
-        //Business logic
-        {script: '../javascripts/canvas.js'},
-        {script: '../javascripts/playback.js'}
+        {script: 'https://cdnjs.cloudflare.com/ajax/libs/tone/13.2.0/Tone.js'}
+        //jQuery
+        // {script: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'},
+        
+       
       ]
   }); 
 });
