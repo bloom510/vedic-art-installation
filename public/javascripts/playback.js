57641da88f5d74e640f89856b1ff196733a14188
bloom510 <bloomjazzer@gmail.com>
2018-07-09 20:55:28 -0700

    Adds comments and methods to playback

--- public/javascripts/playback.js
+++ public/javascripts/playback.js
@@ -1,6 +1,13 @@
 class Player {
     constructor(data){
         console.log(data);
+        //this.play(data)
       
+    }
+    play(data){
+        //start playing
+    }
+    stop(){
+        //stop playing
     }
 }
