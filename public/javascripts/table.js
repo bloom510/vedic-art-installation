560703dc8feeab64475151fc8f6a88ee82855be3
bloom510 <bloomjazzer@gmail.com>
2018-07-11 15:09:54 -0700

    Adds vectors to each rectangle center

--- public/javascripts/table.js
+++ public/javascripts/table.js
@@ -10,11 +10,21 @@ class Table {
     }
     drawCell(x, y){
         // console.log(this.context)
+   
+
+        let centerX = x + (this.windowWidth / this.modulus) / 2;
+        let centerY = y + (this.windowHeight / this.modulus) / 2;
+
+        this.context.beginPath();
+        this.context.arc(centerX,centerY,5,0,2*Math.PI);
+     
+
         this.context.strokeRect(
             x, y, 
             this.windowWidth / this.modulus, 
             this.windowHeight / this.modulus
         );
+        this.context.stroke();
     }
     drawGrid(){
         for(let x = 0; x < this.windowWidth; x += this.windowWidth / this.modulus){
@@ -22,4 +32,5 @@ class Table {
             this.drawCell(x,y)
         }
     }
+
 }
