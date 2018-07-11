43238aad293bfbb9963a0a421c28777f050c08df
bloom510 <bloomjazzer@gmail.com>
2018-07-11 15:56:39 -0700

    Adds vector system

--- public/javascripts/table.js
+++ public/javascripts/table.js
@@ -4,32 +4,50 @@ class Table {
         this.windowWidth = width;
         this.windowHeight = height;
         this.context = context;
+        this.vector = class Vector {
+            constructor(x, y, windowWidth, windowHeight, modulus, context){
+                this.x = x;
+                this.y = y;
+                this.cx = x + (windowWidth / modulus) / 2;
+                this.cy = y + (windowHeight / modulus) / 2;
+                this.context = context;
+                this.draw(windowWidth, windowHeight)
+            }
+            draw(windowWidth, windowHeight){
+                this.context.beginPath();
+                this.context.arc(this.cx,this.cy,3,0,2*Math.PI);
+                this.context.stroke();
+                this.context.closePath();
+            }
+        }
         this.cells = [];
         this.drawGrid();
         
     }
     drawCell(x, y){
-        // console.log(this.context)
-   
-
-        let centerX = x + (this.windowWidth / this.modulus) / 2;
-        let centerY = y + (this.windowHeight / this.modulus) / 2;
-
         this.context.beginPath();
-        this.context.arc(centerX,centerY,3,0,2*Math.PI);
-     
-
-        this.context.strokeRect(
-            x, y, 
-            this.windowWidth / this.modulus, 
-            this.windowHeight / this.modulus
-        );
+        this.context.arc(cx,cy,3,0,2*Math.PI);
+    
         this.context.stroke();
+        this.context.closePath();
     }
     drawGrid(){
+        let cell;
         for(let x = 0; x < this.windowWidth; x += this.windowWidth / this.modulus){
-            for(let y = 0; y < this.windowHeight; y += this.windowHeight / this.modulus)
-            this.drawCell(x,y)
+            for(let y = 0; y < this.windowHeight; y += this.windowHeight / this.modulus){
+            //Draw rectangle
+            this.context.strokeRect(
+                x, y, 
+                this.windowWidth / this.modulus, 
+                this.windowHeight / this.modulus
+            );
+
+            //Plot vector in center of rectangle
+            let vector = new this.vector(x, y, this.windowWidth, this.windowHeight, this.modulus, this.context)
+            vector.draw()
+            this.cells.push(vector)
+            }
+        
         }
     }
 
